056a5b33d19463f8147e70997fb41fc67bab1d19
Julia Heemskerk <juliaheemskerk@icloud.com>
2025-12-25 13:15:24 +0100

    working js
    made calculator work. what to do next:
    - not allow multiple * or / etc
    - not allow a calc to start with * or / etc
    - make delete button instead of clear
    - let last few btns work

--- script.js
+++ script.js
@@ -2,9 +2,31 @@
 
 const hoverSound = new Audio('audio/hover.mp3');
 const clickSound = new Audio('audio/click.mp3');
+let calcWindow;
+let lastCalc
+
+function clearSom(){
+    calcWindow.innerText = "";
+}
+
+function getNumber(num){
+    calcWindow.innerText += num;
+}
+
+function calculate(){
+    try {
+        lastCalc.innerText = eval(calcWindow.innerText);
+        clearSom()
+    } catch (e) {
+        lastCalc.innerText = "Error";
+    }
+}
 
 // wacht tot de DOM helemaal is geladen
 document.addEventListener('DOMContentLoaded', () => {
+    calcWindow = document.getElementById("windowval");
+    lastCalc = document.getElementById("result")
+
     // Target the outer interactive elements so we only play once per hover
     const buttons = document.querySelectorAll('.button, .small-button, .round-button');
     buttons.forEach(button => {
@@ -18,4 +40,4 @@ document.addEventListener('DOMContentLoaded', () => {
             clickSound.play();
         });
     });
-});         
+});
